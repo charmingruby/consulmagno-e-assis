b066b5b85af6cd0dd10f696816d7d0868ac1b051
charmingruby <mrrubycanal3105@gmail.com>
2023-09-11 08:44:43 -0300

    fixed errors to deploy

--- src/app/(app)/components/footer/copyright.tsx
+++ src/app/(app)/components/footer/copyright.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 export function Copyright() {
   return (
-    <div className="text-gray-300 text-sm" >
-      © Copyright 2023, {" "}
-      <Link href='/' prefetch={false} className='underline underline-offset-4'>
+    <div className="text-gray-300 text-sm">
+      © Copyright 2023,{' '}
+      <Link href="/" prefetch={false} className="underline underline-offset-4">
         Consulmagno & Assis Advocacia
-      </Link >
+      </Link>
       . Todos direitos reservados.
-    </div >
+    </div>
   )
 }
